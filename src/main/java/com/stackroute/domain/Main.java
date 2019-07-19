aca08a9fb047d858623c40a785b26d6e1e4872ff
ramitha <ramitha1407@gmail.com>
2019-07-19 10:27:00 +0530

    updated gitignore file and removed unwanted codes

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -1,14 +1,8 @@
 package com.stackroute.domain;
 
-//import org.graalvm.compiler.nodes.NodeView;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
+
 
 public class Main {
     public static void main(String args[])
@@ -26,20 +20,5 @@ public class Main {
 
 
 
-//        BeanFactory factory=new ClassPathXmlApplicationContext("beans.xml");
-//        movie=factory.getBean("movie",Movie.class);
-//        movie.display();
-
-//        DefaultListableBeanFactory beanFactory=new DefaultListableBeanFactory();
-//        BeanDefinitionReader reader=new XmlBeanDefinitionReader(beanFactory);
-//        reader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
-//        Movie movie1=(Movie)beanFactory.getBean("movie");
-//        movie1.display();
-//
-//        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
-//        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
-//        beanDefinitionReader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
-//        Movie moviebean1=(Movie)beanFactory.getBean("movie");
-//        moviebean1.display();
     }
 }
